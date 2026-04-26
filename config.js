9527c7ca73eec5fc1d6e3dd3183b70f4db6a48ae
Lobi Blümelein <leano.matschbeere@gmail.com>
2026-04-26 11:15:08 +0200

    Add configuration settings for Supabase

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://yqgtlfbykubjizqghiat.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxZ3RsZmJ5a3Viaml6cWdoaWF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNDQ0NzIsImV4cCI6MjA5MjcyMDQ3Mn0.qY9zREp-l_dpKJT1fnX6tiYKdsUwv0QNLezbUTkwTho',
+    MAX_RETRIES: 3,
+    RETRY_DELAY: 2000,
+    REQUEST_TIMEOUT: 10000
+};
+console.log('[CONFIG] Geladen');
